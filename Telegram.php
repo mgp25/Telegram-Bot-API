8e3b5806602fd597e405f906282c86f2ab519305
mgp25 <me@mgp25.com>
2015-07-14 01:55:42 +0200

    little fix
    I have to rewrite everything, this is a test!

--- Telegram.php
+++ Telegram.php
@@ -9,21 +9,22 @@ class telegramBot
   public function __construct($token)
   {
     $this->token = $token;
+    $this->baseURL = BASE_URL . $this->token . DIRECTORY_SEPARATOR;
   }
 
   function pollUpdates($offset, $timeout = 60, $limit = 100)
   {
-    return json_decode(file_get_contents(BASE_URL . "getUpdates?offset=$offset&timeout=$timeout&limit=$limit"), true);
+    return json_decode(file_get_contents($this->baseURL . "getUpdates?offset=$offset&timeout=$timeout&limit=$limit"), true);
   }
 
   function sendMessage($chatID, $text)
   {
-    return json_decode(file_get_contents(BASE_URL . "sendMessage?chat_id=$chatID&text=" . urlencode($text)), true);
+    return json_decode(file_get_contents($this->baseURL . "sendMessage?chat_id=$chatID&text=" . urlencode($text)), true);
   }
 
   function forwardMessage($chatID, $fromChatID, $messageID)
   {
-    return json_decode(file_get_contents(BASE_URL . "forwardMessage?chat_id=$chatID&from_chat_id=$fromChatID&message_id=$messageID"), true);
+    return json_decode(file_get_contents($this->baseURL . "forwardMessage?chat_id=$chatID&from_chat_id=$fromChatID&message_id=$messageID"), true);
   }
 
 }
