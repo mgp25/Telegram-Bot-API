c4111b5da7f5f2ae495911a0e33a1928f63a81dc
mgp25 <me@mgp25.com>
2015-07-14 19:04:51 +0200

    rewritten function + sendPhoto added
    sendPhoto not working 100% yet, still have issues with url images

--- Telegram.php
+++ Telegram.php
@@ -12,44 +12,118 @@ class telegramBot
     $this->baseURL = self::BASE_URL . $this->token . DIRECTORY_SEPARATOR;
   }
 
-  public function pollUpdates($offset, $timeout = 60, $limit = 100)
+  /**
+   * A simple method for testing your bot's auth token.
+   * Returns basic information about the bot in form of a User object.
+   *
+   * @link https://core.telegram.org/bots/api#getme
+   *
+   * @return Array
+   */
+  public function getMe()
   {
-    $params = array(
-      'offset' => $offset
-    );
+    return $this->sendRequest('getMe', null);
+  }
 
-    if ($timeout != 60)
-      $params['timeout'] = $timeout;
-    if ($limit != 100)
-      $params['limit'] = $limit;
+
+  /**
+   * Use this method to receive incoming updates using long polling.
+   *
+   * @link https://core.telegram.org/bots/api#getupdates
+   *
+   * @param int $offset
+   * @param int $limit
+   * @param int $timeout
+   *
+   * @return Array
+   */
+  public function pollUpdates($offset, $timeout = null, $limit = null)
+  {
+    $params = compact('offset', 'limit', 'timeout');
 
     return $this->sendRequest('getUpdates', $params);
   }
 
-  public function sendMessage($chatID, $text)
+  /**
+   * Send text messages.
+   *
+   * @link https://core.telegram.org/bots/api#sendmessage
+   *
+   * @param int            $chat_id
+   * @param string         $text
+   * @param bool           $disable_web_page_preview
+   * @param int            $reply_to_message_id
+   * @param KeyboardMarkup $reply_markup
+   *
+   * @return Array
+   */
+  public function sendMessage($chat_id, $text, $disable_web_page_preview = false, $reply_to_message_id = null, $reply_markup = null)
   {
-    $params = array(
-      'chat_id' => $chatID,
-      'text'    => $text
-    );
+    $params = compact('chat_id', 'text', 'disable_web_page_preview', 'reply_to_message_id', 'reply_markup');
 
     return $this->sendRequest('sendMessage', $params);
   }
 
-  public function forwardMessage($chatID, $fromChatID, $messageID)
+  /**
+   * Forward messages of any kind.
+   *
+   * @link https://core.telegram.org/bots/api#forwardmessage
+   *
+   * @param int $chat_id
+   * @param int $from_chat_id
+   * @param int $message_id
+   *
+   * @return Array
+   */
+  public function forwardMessage($chat_id, $from_chat_id, $from_chat_id)
   {
-    $params = array(
-      'chat_id'      => $chatID,
-      'from_chat_id' => $fromChatID,
-      'message_id'   => $messageID
-    );
+    $params = compact('chat_id', 'from_chat_id', 'from_chat_id');
 
     return $this->sendRequest('forwardMessage', $params);
   }
 
-  public function sendRequest($method, $params)
+  /**
+   * Send Photos.
+   *
+   * @link https://core.telegram.org/bots/api#sendphoto
+   *
+   * @param int            $chat_id
+   * @param string         $photo
+   * @param string         $caption
+   * @param int            $reply_to_message_id
+   * @param KeyboardMarkup $reply_markup
+   *
+   * @return Array
+   */
+  public function sendPhoto($chat_id, $photo, $caption = null, $reply_to_message_id = null, $replyMarkup = null)
   {
+    $data = compact('chat_id', 'photo', 'caption', 'reply_to_message_id', 'reply_markup');
+
+    if (((!is_dir($photo)) && (filter_var($url, FILTER_VALIDATE_URL) === FALSE)))
+      return $this->sendRequest('sendPhoto', $data);
+
+    return $this->uploadFile('sendPhoto', $data);
+  }
+
+  private function sendRequest($method, $params)
+  {
+    if ($method == 'getMe')
+      return json_decode(file_get_contents($this->baseURL . $method), true);
+
     return json_decode(file_get_contents($this->baseURL . $method . '?' . http_build_query($params)), true);
+  }
+
+  private function uploadFile($method, $data)
+  {
+    $finfo = finfo_open(FILEINFO_MIME_TYPE);
+    $mime_type = finfo_file($finfo, $data['photo']);
+    $data['photo'] = new CurlFile($data['photo'], $mime_type, $data['photo']);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $this->baseURL . $method);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+    return json_decode(curl_exec($ch), true);
   }
 
 }
