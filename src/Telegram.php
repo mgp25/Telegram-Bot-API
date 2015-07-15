b0da400cad44a312a3e647f95bf78bea91d88a2b
mgp25 <me@mgp25.com>
2015-07-15 03:01:48 +0200

    More functions added

--- src/Telegram.php
+++ src/Telegram.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'Exception.php';
+
 class telegramBot
 {
   const BASE_URL = 'https://api.telegram.org/bot';
@@ -104,6 +106,208 @@ class telegramBot
 
     return $this->uploadFile('sendPhoto', $data);
   }
+
+  /**
+   * Send Audio.
+   *
+   * @link https://core.telegram.org/bots/api#sendaudio
+   *
+   * @param int            $chat_id
+   * @param string         $audio
+   * @param int            $reply_to_message_id
+   * @param KeyboardMarkup $reply_markup
+   *
+   * @return Array
+   */
+  public function sendAudio($chat_id, $audio, $reply_to_message_id = null, $replyMarkup = null)
+  {
+    $data = compact('chat_id', 'audio', 'reply_to_message_id', 'reply_markup');
+
+    if (((!is_dir($audio)) && (filter_var($audio, FILTER_VALIDATE_URL) === FALSE)))
+      return $this->sendRequest('sendAudio', $data);
+
+    return $this->uploadFile('sendAudio', $data);
+  }
+
+  /**
+   * Send Document.
+   *
+   * @link https://core.telegram.org/bots/api#senddocument
+   *
+   * @param int            $chat_id
+   * @param string         $document
+   * @param int            $reply_to_message_id
+   * @param KeyboardMarkup $reply_markup
+   *
+   * @return Array
+   */
+  public function sendDocument($chat_id, $document, $reply_to_message_id = null, $replyMarkup = null)
+  {
+    $data = compact('chat_id', 'document', 'reply_to_message_id', 'reply_markup');
+
+    if (((!is_dir($document)) && (filter_var($document, FILTER_VALIDATE_URL) === FALSE)))
+      return $this->sendRequest('sendDocument', $data);
+
+    return $this->uploadFile('sendDocument', $data);
+  }
+
+  /**
+   * Send Sticker.
+   *
+   * @link https://core.telegram.org/bots/api#sendsticker
+   *
+   * @param int            $chat_id
+   * @param string         $sticker
+   * @param int            $reply_to_message_id
+   * @param KeyboardMarkup $reply_markup
+   *
+   * @return Array
+   */
+  public function sendSticker($chat_id, $sticker, $reply_to_message_id = null, $replyMarkup = null)
+  {
+    $data = compact('chat_id', 'sticker', 'reply_to_message_id', 'reply_markup');
+
+    if (((!is_dir($sticker)) && (filter_var($sticker, FILTER_VALIDATE_URL) === FALSE)))
+      return $this->sendRequest('sendSticker', $data);
+
+    return $this->uploadFile('sendSticker', $data);
+  }
+
+  /**
+   * Send Video.
+   *
+   * @link https://core.telegram.org/bots/api#sendvideo
+   *
+   * @param int            $chat_id
+   * @param string         $video
+   * @param int            $reply_to_message_id
+   * @param KeyboardMarkup $reply_markup
+   *
+   * @return Array
+   */
+  public function sendVideo($chat_id, $video, $reply_to_message_id = null, $replyMarkup = null)
+  {
+    $data = compact('chat_id', 'video', 'reply_to_message_id', 'reply_markup');
+
+    if (((!is_dir($video)) && (filter_var($video, FILTER_VALIDATE_URL) === FALSE)))
+      return $this->sendRequest('sendVideo', $data);
+
+    return $this->uploadFile('sendVideo', $data);
+  }
+
+  /**
+   * Send Location.
+   *
+   * @link https://core.telegram.org/bots/api#sendlocation
+   *
+   * @param int            $chat_id
+   * @param float          $latitude
+   * @param float          $longitude
+   * @param int            $reply_to_message_id
+   * @param KeyboardMarkup $reply_markup
+   *
+   * @return Array
+   */
+  public function sendLocation($chat_id, $latitude, $longitude, $reply_to_message_id = null, $reply_markup = null)
+  {
+    $params = compact('chat_id', 'latitude', 'longitude', 'reply_to_message_id', 'reply_markup');
+
+    return $this->sendRequest('sendLocation', $params);
+  }
+
+  /**
+   * Send Chat Action.
+   *
+   * @link https://core.telegram.org/bots/api#sendchataction
+   *
+   * @param int            $chat_id
+   * @param string         $action
+   *
+   * @return Array
+   */
+  public function sendChatAction($chat_id, $action)
+  {
+    $actions = array(
+    'typing',
+    'upload_photo',
+    'record_video',
+    'upload_video',
+    'record_audio',
+    'upload_audio',
+    'upload_document',
+    'find_location',
+    );
+    if (isset($action) && in_array($action, $actions))
+    {
+      $params = compact('chat_id', 'action');
+      return $this->sendRequest('sendChatAction', $params);
+    }
+
+    throw new TelegramException('Invalid Action! Accepted value: '.implode(', ', $actions));
+  }
+
+  /**
+   * Get user profile photos.
+   *
+   * @link https://core.telegram.org/bots/api#getsserprofilephotos
+   *
+   * @param int            $chat_id
+   * @param int            $offset
+   * @param int            $limit
+   *
+   * @return Array
+   */
+  public function getUserProfilePhotos($user_id, $offset = null, $limit = null)
+  {
+        $param = compact('user_id', 'offset', 'limit');
+
+        return $this->sendRequest('getUserProfilePhotos', $params);
+  }
+
+ /**
+  * Set a Webhook to receive incoming updates via an outgoing webhook.
+  *
+  * @param string $url HTTPS url to send updates to.
+  *
+  * @return Array
+  *
+  */
+ public function setWebhook($url)
+ {
+   if (filter_var($url, FILTER_VALIDATE_URL) === false)
+     throw new TelegramException('Invalid URL provided');
+
+   if (parse_url($url, PHP_URL_SCHEME) !== 'https')
+      throw new TelegramException('Invalid URL, it should be a HTTPS url.');
+
+   return $this->sendRequest('setWebhook', $url);
+}
+
+ /**
+  * Returns webhook updates sent by Telegram.
+  * Works only if you set a webhook.
+  *
+  * @see setWebhook
+  *
+  * @return Array
+  */
+ public function getWebhookUpdates()
+ {
+   $body = json_decode(file_get_contents('php://input'), true);
+
+   return $body;
+ }
+
+ /**
+  * Removes the outgoing webhook.
+  *
+  * @return Array
+  */
+ public function removeWebhook()
+ {
+   $url = '';
+   return $this->sendRequest('setWebhook', compact('url'));
+ }
 
   private function sendRequest($method, $params)
   {
